2a2e36e5a9a76d551df634f78ec5fd56e104592a
Motti789 <mchillywilly@gmail.com>
2021-07-19 13:58:55 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,10 +3,16 @@ class CashRegister
 
     def initialize(total = 0)
         @total = total
-        @discount = discount
+        discount 
     end
 
     def total
         @total
     end    
+
+    def add_item(title, price, amount)
+        "#{title}, #{price}"
+        self.total += 15
+
+    end   
 end        
